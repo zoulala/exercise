42f37f530d0c26803fbf22b43e02a2e0057d0a32
zoulingwei <zoulingwei@cyou-inc.com>
2019-02-27 15:46:51 +0800

    day day up

--- leetcode/DP_minPathSum_2D.py
+++ leetcode/DP_minPathSum_2D.py
@@ -25,14 +25,14 @@ def minPathSum( grid):
     dp = grid[:]
 
     cs, rs = 0, 0
-    for i in range(n):
+    for i in range(n):  # 获取第一行的dp值
         cs += grid[0][i]
         dp[0][i] = cs
-    for i in range(m):
+    for i in range(m):  # 获取第一列的dp值
         rs += grid[i][0]
         dp[i][0] = rs
 
-    for i in range(1, m):
+    for i in range(1, m):  # 获取剩下的dp值，并返回最后一个dp值即为所求
         for j in range(1, n):
             dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
     return dp[m - 1][n - 1]
