e28155cdbeec410b59dd91113ba14a8cb1cc2e98
zoulala <zlw2008ok@126.com>
2019-10-27 14:32:02 +0800

    add argv_test

---
+++ structer-test/argv_test.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+#
+# *****************************************************
+#
+# file:     argv_test.py
+# author:   zoulingwei@zuoshouyisheng.com
+# date:     2019-10-27
+# brief:    
+#
+# cmd>e.g:  
+# *****************************************************
+
+import sys
+import argparse
+
+
+# ------------1-----------
+argvs = sys.argv
+print(argvs[0])  # 脚本名
+'''
+argv_test.py
+'''
+
+# -------------2 ----------  # python argv_test.py --log_path='abc/def/' --flag 3 --port=8080 -l
+parser = argparse.ArgumentParser(usage="it's usage test.", description="help info.")
+parser.add_argument("--log_path", default='./', help="the logs path.")
+parser.add_argument("--flag", type=int, choices=[0, 1, 2, 3], default=0, help="test choices")
+parser.add_argument("-p","--port", type=int, required=True, help="the port number.")
+parser.add_argument("-l", "--log", default=False, action="store_true", help="active log info.")
+parser.add_argument("-r", "--rog", default=False, action="store_true", help="active log info.")
+
+args = parser.parse_args()
+
+print(args.log_path)
+print(args.flag)
+print(args.port)
+print(args.log)
+print(args.rog)
+
+
+'''
+abc/def/
+3
+8080
+True
+False
+'''
--- structer-test/xml_parser.py
+++ structer-test/xml_parser.py
@@ -14,31 +14,45 @@
 import os
 import xmltodict
 
-sf = open('partment.txt','w')
+sf = open('hospital.txt','w')
 n = 1
-path = '门诊病历导出数据'
+path = 'hospital/23280840'
 
 files = os.listdir(path)
 for file in files:
     filename = os.path.join(path,file)
     text = ''
     with open(filename, 'r') as f:
-        try:
-            for line in f:
+        line = f.read()
+        dic_data = xmltodict.parse(line)
 
-                dic_data = xmltodict.parse(line)
+        for base_key in dic_data:
 
-                for base_key in dic_data:
-
-                    for code_key in dic_data[base_key]:
-                        element_line = dic_data[base_key][code_key]
-                        if '@value' in element_line and element_line['@value']:
-                            key = element_line['@dataelementName'].split()[-1]
-                            value = element_line['@value']
-                            # print(key,' : ',value)
-                            text += key+':'+value+'\t'
-        except:
-            continue
+            for code_key in dic_data[base_key]:
+                element_line = dic_data[base_key][code_key]
+                if '@displayName' in element_line:
+                    print(element_line['@displayName'])
+                if '@value' in element_line and element_line['@value']:
+                    key = element_line['@dataelementName'].split()[-1]
+                    value = element_line['@value']
+                    # print(key,' : ',value)
+                    text += '<'+key +'>'+ ':' + value + '\t'
+        # try:
+        #     for line in f:
+        #
+        #         dic_data = xmltodict.parse(line)
+        #
+        #         for base_key in dic_data:
+        #
+        #             for code_key in dic_data[base_key]:
+        #                 element_line = dic_data[base_key][code_key]
+        #                 if '@value' in element_line and element_line['@value']:
+        #                     key = element_line['@dataelementName'].split()[-1]
+        #                     value = element_line['@value']
+        #                     # print(key,' : ',value)
+        #                     text += key+':'+value+'\t'
+        # except:
+        #     continue
 
     if text:
         n += 1
